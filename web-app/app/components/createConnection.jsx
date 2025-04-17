0468f747d508110b9da059d7c1b23e81d19dfa04
Michael Blanton <blanton@xiggit.com>
2025-04-17 10:54:18 -0700

    Implement code changes to enhance functionality and improve performance

--- web-app/app/components/createConnection.jsx
+++ web-app/app/components/createConnection.jsx
@@ -1,4 +1,21 @@
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
+import _ from 'lodash';
+import { use } from 'react';
+
 const CreateConnection = () => {
+  const [isReady, setIsReady] = useState(false);
+  const {accessToken} = useSelector((state) => state.session);
+
+  useEffect(() => {
+    if (!accessToken) {
+      setIsReady(true);
+    }
+  }, [accessToken]);
+
+  if (!_.isNil(accessToken)) {
+    return;
+  }
 }
 
 export default CreateConnection;
